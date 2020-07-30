1379ddceb2bbeb064dd28eee3ccd68bdd9e75522
天火流光 <945076636@qq.com>
2020-07-30 22:08:45 +0800

    指定网址打开
    Signed-off-by: 天火流光 <945076636@qq.com>

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Plug-in
+"browser_action": {
+        "default_icon": "img/icon.png",
+        "default_popup": "src/views/popup.html"
+    },
--- ymx_crx_spider/src/js/background.js
+++ ymx_crx_spider/src/js/background.js
@@ -1,11 +1,25 @@
-console.log("......");
+// console.log("......");
 
-//
-chrome.webRequest.onCompleted.addListener(
-  function (detail) {
-    console.log(detail);
-  },
-  {
-    urls: ["<all_urls>"],
-  }
-);
+// //
+// chrome.webRequest.onCompleted.addListener(
+//   function (detail) {
+//     console.log(detail);
+//   },
+//   {
+//     urls: ["<all_urls>"],
+//   }
+// );
+
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          // 只有打开 amazon 才显示 pageAction
+          new chrome.declarativeContent.PageStateMatcher({ pageUrl: { urlContains: 'amazon' } })
+        ],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+      }
+    ]);
+  });
+});
